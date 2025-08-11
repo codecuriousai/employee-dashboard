7163e53a8693696f5dfb1bb73a030849c34974b8
codecuriousai <codecuriousai@gmail.com>
2025-08-11 09:06:47 +0530

    actions config changes

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,26 @@
 module.exports = {
-    collectCoverage: true,
-    coverageDirectory: "coverage",
-    collectCoverageFrom: [
-      "src/**/*.{js,jsx,ts,tsx}",
-      "!src/**/*.d.ts",
-      "!src/index.js"
-    ],
-    coverageReporters: ["json", "lcov", "text", "clover", "html"],
-    coverageThreshold: {
-      global: {
-        branches: 80,
-        functions: 80,
-        lines: 80,
-        statements: 80
-      }
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/index.js",
+    "!src/**/__tests__/**",  // exclude test folders
+    "!src/**/?(*.)+(spec|test).[jt]s?(x)" // exclude test files
+  ],
+  coverageReporters: [
+    "json",    // for machine-readable
+    "lcov",    // for coverage tools
+    "text",    // summary in console
+    "clover",  // alternative XML format
+    "html"     // for nice local / artifact viewing
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
     }
-  };
+  }
+};
